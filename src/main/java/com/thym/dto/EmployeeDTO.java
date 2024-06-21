977e710b20fd174c91789f97ec736b08b9719cb1
sue <ksm7058@naver.com>
2024-06-21 18:18:14 +0900

    Update EmployeeDTO.java

--- src/main/java/com/thym/dto/EmployeeDTO.java
+++ src/main/java/com/thym/dto/EmployeeDTO.java
@@ -1,5 +1,72 @@
 package com.thym.dto;
 
-public class EmployeeDTO {
+import org.apache.ibatis.type.Alias;
 
+@Alias("employee")
+public class EmployeeDTO {
+	private String eno;
+	private String name;
+	private String department;
+	private int positionNo;
+	private int salary;
+
+	public EmployeeDTO() {
+	}
+
+	public EmployeeDTO(String eno, String name, String department, int positionNo, int salary) {
+		this.eno = eno;
+		this.name = name;
+		this.department = department;
+		this.positionNo = positionNo;
+		this.salary = salary;
+	}
+
+	public String getEno() {
+		return eno;
+	}
+
+	public void setEno(String eno) {
+		this.eno = eno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public int getPositionNo() {
+		return positionNo;
+	}
+
+	public void setPositionNo(int positionNo) {
+		this.positionNo = positionNo;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeDTO [eno=" + eno + ", name=" + name + ", department=" + department + ", positionNo="
+				+ positionNo + ", salary=" + salary + "]";
+	}
+
+	
+	
 }
